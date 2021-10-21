633f5be40cab02a0598df7c960dbdf2c4aa5e54a
Alfons Fanta <92315660+AlfonsFanta@users.noreply.github.com>
2021-10-21 20:24:12 +0200

    Added date, chalk and got

--- index.js
+++ index.js
@@ -7,6 +7,70 @@
 
 // und das ganze noch einmal
 
-// wow, so viele Infos
-// nun added Mathias ein comment
-// noch ein Kommentar!
+import {
+  compareAsc,
+  format,
+} from 'date-fns';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+format(new Date(2014, 1, 11), 'yyyy-MM-dd')
+//=> '2014-02-11'
+
+const dates = [
+  new Date(2021, 6, 2),
+  new Date(2021, 1, 11),
+  new Date(2021, 6, 10),
+]
+dates.sort(compareAsc)
+//=> [
+//   Wed Feb 11 1987 00:00:00,
+//   Mon Jul 10 1989 00:00:00,
+//   Sun Jul 02 1995 00:00:00
+// ]
+
+console.log('These are the dates:', dates);
+
+const chalk = require('chalk');
+const log = console.log;
+
+// Combine styled and normal strings
+log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+
+// Compose multiple styles using the chainable API
+log(chalk.blue.bgRed.bold('Hello world!'));
+
+// Pass in multiple arguments
+log(chalk.blue('Hello', 'World!', 'Foo', 'bar', 'biz', 'baz'));
+
+// Nest styles
+log(chalk.red('Hello', chalk.underline.bgBlue('world') + '!'));
+
+// Nest styles of the same type even (color, underline, background)
+log(chalk.green(
+	'I am a green line ' +
+	chalk.blue.underline.bold('with a blue substring') +
+	' that becomes green again!'
+));
+
+// ES2015 template literal
+log(`
+CPU: ${chalk.red('90%')}
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`);
+
+const got = require('got');
+
+(async () => {
+	const {body} = await got.post('https://httpbin.org/anything', {
+		json: {
+			hello: 'world'
+		},
+		responseType: 'json'
+	});
+
+	console.log(body.data);
+	//=> {hello: 'world'}
+})();
